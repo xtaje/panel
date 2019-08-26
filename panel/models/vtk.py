02fad2ee7becb658c3f4cd14f0e41a92c1624184
Mateusz Paprocki <mattpap@gmail.com>
2019-08-27 01:09:55 +0200

    Drop CUSTOM_MODELS

--- panel/models/vtk.py
+++ panel/models/vtk.py
@@ -2,12 +2,8 @@
 """
 Defines custom VTKPlot bokeh model to render VTK objects.
 """
-import os
-
 from bokeh.core.properties import String, Bool, Dict, Any, Override
 from bokeh.models import HTMLBox
-
-from ..compiler import CUSTOM_MODELS
 
 vtk_cdn = "https://unpkg.com/vtk.js@8.3.15/dist/vtk.js"
 
@@ -23,7 +19,7 @@ class VTKPlot(HTMLBox):
     __js_require__ = {"paths": {"vtk": vtk_cdn[:-3]},
                       "shim": {"vtk": {"exports": "vtk"}}}
 
-    __implementation__ = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'vtk.ts')
+    __implementation__ = 'vtk.ts'
 
     append = Bool(default=False)
 
@@ -36,7 +32,3 @@ class VTKPlot(HTMLBox):
     height = Override(default=300)
 
     width = Override(default=300)
-
-
-
-CUSTOM_MODELS['panel.models.plots.VTKPlot'] = VTKPlot
